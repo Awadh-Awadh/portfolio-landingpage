a101261549dde45b47906ce5279ce136694dbcff
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-06-06 18:25:37 +0300

    add fading effect to the sections

---
+++ main.js
@@ -0,0 +1,15 @@
+ScrollOut({  
+	targets: "#about",
+	threshold:0.4,
+	once: true
+});;
+ScrollOut({  
+	targets: "#projects",
+	threshold:0.4,
+	once: true
+});;
+ScrollOut({  
+	targets: "#contact",
+	threshold:0.4,
+	once: true
+});;
